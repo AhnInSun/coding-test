beaa59d60f61f0e7fb181bfff204db30673aa53d
AhnInSun <informationx@naver.com>
2020-12-21 21:12:57 +0900

    GitHub Test

---
+++ JavaProject/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		/*
+		    4 5
+		 
+		    1 3 6 10 14
+			2 5 9 13 17
+			4 8 12 16 19
+			7 11 15 18 20
+		*/
+		
+		int[][] arr = new int[n][m];
+		
+		for (int i = 0; i < n; i++) {
+			
+			for (int j = 0; j < m; j++) {
+				
+			}
+			
+			System.out.println("");
+		}
+	}	
+} 
