359fb0928c4da565be22bd64ddc7557018e82bc1
ahninsun <informationx@naver.com>
2021-01-03 13:10:10 +0900

    CodeUp > 2차원배열 빗금채우기 3-1

--- coding-test-project/src/Main.java
+++ coding-test-project/src/Main.java
@@ -1,11 +1,23 @@
 import kr.codeup.CodeUpArray;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		int[] rtn = CodeUpArray.array3_1();
 
-		for (int i = 0; i < rtn.length; i++) {
-			System.out.println(rtn[i]);
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		int[][] rtnArr = CodeUpArray.array3_1(n, m);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(rtnArr[i][j] + " ");
+			}
+			System.out.println("");
 		}
+
 	}
 }
--- coding-test-project/src/kr/codeup/CodeUpArray.java
+++ coding-test-project/src/kr/codeup/CodeUpArray.java
@@ -2,9 +2,25 @@ package kr.codeup;
 
 public class CodeUpArray {
 
+
+
     // 2차원 배열 빗금 채우기 3-1
-    public static int[] array3_1(){
-        int[] rtn = {1,2,3,4};
-        return rtn;
+    public static int[][] array3_1(int n, int m) {
+
+        int[][] rtnArr = new int[n][m];
+        int cnt = 1;
+
+        for (int i = 0; i < n+m-1; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (j + k == i) {
+                        rtnArr[k][j] = cnt++;
+                        break;
+                    }
+                }
+            }
+        }
+        return rtnArr;
     }
+
 }
